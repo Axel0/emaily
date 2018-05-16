b8e6785e13d45d9e93893ec0917b38de00d2534d
Alessandro Carrese <alessandro@imacdiaessandro.homenet.telecomitalia.it>
2018-05-16 22:43:27 +0200

    backup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 class Header extends Component {
+
+  renderContent(){
+    switch(this.props.auth){
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Log In With Google</a></li>
+        )
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render(){
     return(
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys':'/'} 
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
 
           <ul className="right">
-            <a>Login with Google</a>
+            <li>
+              {this.renderContent()}
+            </li>
           </ul>
         </div>
       </nav>
@@ -18,4 +38,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({auth}){
+
+  return {auth};
+
+}
+
+
+
+export default connect(mapStateToProps)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import materializeCss from 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk'
 import App from './components/App';
-
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 
-const store=createStore(reducers,{}, applyMiddleware());
+const store=createStore(reducers,{}, applyMiddleware(reduxThunk));
 
 
 ReactDOM.render(
